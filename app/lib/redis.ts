7b6c7abfc2c96957d8929aa416c841a098874c3b
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2025-01-13 22:05:36 -0800

    Update environment variables and metadata for Ninja Belt Tracker app

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -3,8 +3,8 @@ import { students } from "../types";
 
 const getRedisConfig = () => {
   return {
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_READ_ONLY_TOKEN,
   };
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
   },
 };
 
